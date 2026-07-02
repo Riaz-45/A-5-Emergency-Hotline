57cfa77f7662ec9c0bf4e80c6186d9734359e6cd
Riaz-45 <rakibulhasanriaz1234@gmail.com>
2026-07-03 00:56:37 +0600

    added copy button alert and increase the copy count and also hotline number is copied on clink and paste it anywhere

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 let count = 0;
-// document.getElementById('heart-btn').addEventListener('click', function(){
-//     // console.log('clicked')
-
-//     count ++;
-    
-//     return document.getElementById('heart-btn-count').innerText = count;
-    
-
-// })
-
-// document.getElementById('heart-btn-2').addEventListener('click', function(){
-//     // console.log('clicked')
-
-//     count ++;
-
-//     return document.getElementById('heart-btn-count').innerText = count;
-
 
 const hearts = document.getElementsByClassName('heart-btn');
 for(const heart of hearts){
     heart.addEventListener('click', function(){
         count ++;
         return document.getElementById('heart-btn-count').innerText = count;
+    })
+}
+
+
+const copies = document.getElementsByClassName('copy-btn');
+for(const copy of copies){
+    copy.addEventListener('click', function(){
+        const card = copy.closest('.card');
+        const hotlineNumber = card.querySelector('.hotline-number').innerText;
+        navigator.clipboard.writeText(hotlineNumber)
+
+        count++;
+
+        document.getElementById('copy-count').innerText = count;
+
+        alert('the number is copied: ' + hotlineNumber);
     })
 }
