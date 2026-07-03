a2a3d05df239f17d5f037c4d48b3e27ecf1adc22
Riaz-45 <rakibulhasanriaz1234@gmail.com>
2026-07-03 20:38:38 +0600

    added service name, service number and live time in call history section and also clear all heastory

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let count = 0;
 let coinValue = 100;
 
+// for increasing heart count number
+
 const hearts = document.getElementsByClassName('heart-btn');
 for(const heart of hearts){
     heart.addEventListener('click', function(){
@@ -8,6 +10,9 @@ for(const heart of hearts){
         return document.getElementById('heart-btn-count').innerText = count;
     })
 }
+
+
+// call button and call history functionalities
 
 let coinCount = parseInt(document.getElementById('coin-count').innerText);
 const calls = document.getElementsByClassName('call-btn');
@@ -27,11 +32,31 @@ for(const call of calls){
 
         document.getElementById('coin-count').innerText = coinValue;
 
+        // const currentTime = new Date().toLocaleDateString();
+        const currentTime = new Date().toLocaleTimeString();
+        const calHistory = document.getElementById('call-history');
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+        <div class="hind-madurai flex justify-between items-center p-4 mt-4 rounded-lg bg-[rgba(250,250,250,1)]">
+                <div class="font-semibold text-[18px]">
+                    <h2>${serviceName}</h2>
+                    <p class="text-[rgba(92,92,92,1)] text-[18px]">${hotlineNumber}</p>
+                </div>
+                <div>
+                    <p class="text-[18px]">${currentTime}</p>
+                </div>
+            </div>
+        `
+
+        calHistory.appendChild(div);
         
         
     })
 }
 
+
+// for copy button functionalities
 
 const copies = document.getElementsByClassName('copy-btn');
 for(const copy of copies){
