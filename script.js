27430d70b287e8192420cc17063c1a5744ffe559
Riaz-45 <rakibulhasanriaz1234@gmail.com>
2026-07-03 10:31:31 +0600

    added call button alert with service name and number and also reduce the coin after each call click and terminate the process while the coin is less than 20

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 let count = 0;
+let coinValue = 100;
 
 const hearts = document.getElementsByClassName('heart-btn');
 for(const heart of hearts){
     heart.addEventListener('click', function(){
         count ++;
         return document.getElementById('heart-btn-count').innerText = count;
+    })
+}
+
+let coinCount = parseInt(document.getElementById('coin-count').innerText);
+const calls = document.getElementsByClassName('call-btn');
+for(const call of calls){
+    call.addEventListener('click', function(){
+        const card = call.closest('.card');
+        const serviceName = card.querySelector('.service-name').innerText;
+        const hotlineNumber = card.querySelector('.hotline-number').innerText;
+        // alert('🚨' + serviceName + ' service: ' + hotlineNumber);
+        alert(`🚨 ${serviceName} service: ${hotlineNumber}`);
+
+        coinValue -= 20;
+        if(coinValue < 20){
+            alert('❌ insufficient coin');
+            return;
+        }
+
+        document.getElementById('coin-count').innerText = coinValue;
+
+        
+        
     })
 }
 
@@ -20,6 +44,6 @@ for(const copy of copies){
 
         document.getElementById('copy-count').innerText = count;
 
-        alert('the number is copied: ' + hotlineNumber);
+        alert('📞 the number is copied: ' + hotlineNumber);
     })
 }
